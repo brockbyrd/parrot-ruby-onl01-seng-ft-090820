98c647ac4b3ccd2582631ef29335a0077a899404
Brock Byrd <brockbyrdd@gmail.com>
2020-09-08 18:18:40 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase = "Pretty bird!")
+def parrot(phrase = "Squawk!")
   puts "Squawk!"
   "Squawk!"
-  puts "#{phrase}"
-  return "#{phrase}"
+  puts phrase
+  return phrase
 end
